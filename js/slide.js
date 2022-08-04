cf426eaf38450ec0710b01c95e729a3453de96de
pmq2212@gmail.com <pmq2212@gmail.com>
2022-08-04 22:51:22 +0700

    Update content

---
+++ js/slide.js
@@ -0,0 +1,10 @@
+(function ($){
+    $(document).ready(function(){
+        let slider_content = `
+        <span>WE’RE GETTING MARRIED</span>
+        <h4>Save Our Date</h4>
+        <p>28 Tháng 8 2022</p>
+        `
+        $('.slider-content--inner').html(slider_content)
+    })
+})(jQuery)
