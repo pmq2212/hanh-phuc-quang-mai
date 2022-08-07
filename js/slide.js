81498094d670bb00b301bc36d92e822bab52b7bd
pmq2212@gmail.com <pmq2212@gmail.com>
2022-08-07 12:26:14 +0700

    Update content

--- js/slide.js
+++ js/slide.js
@@ -1,9 +1,9 @@
 (function ($){
     $(document).ready(function(){
         let slider_content = `
-        <span>CHÚNG TÔI SẼ VỀ CHUNG MỘT NHÀ</span>
-        <h4>Lưu Giữ Ngày Này</h4>
-        <p>28 Tháng 8 2022</p>
+        <span>WELCOME TO OUR BEGINNING</span>
+        <h4>to have and to hold<br>from this day forward</h4>
+        <p>28 - 08 - 2022</p>
         `
         $('.slider-content--inner').html(slider_content)
     })
